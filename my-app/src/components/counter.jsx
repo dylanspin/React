43ed9bd618615cb2261916f5b07cf1f2bc4fa9d9
Dylan Spin <dylanspin100@hotmail.com>
2021-01-11 15:22:39 +0100

    Tuturial klaar
    Ga nu nog bootstrap weer opnieuw leren en dan met react proberen de oefen examen temaken

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,33 +1,20 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.value,
-    };
-
-    handleIncrement = amount =>{
-        console.log(amount);
-        this.setState({ value: this.state.value + 1})
-    };
-
-    doHandleIncrement = () => {
-        this.handleIncrement({id: 1});
-    };
-
 
     render(){
         return (
           <div>
               {this.props.children}
-              <span className={this.getBadgeClasses()}>{this.state.value}</span>
+              <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
               <button
-                  onClick={() => this.handleIncrement(1)}
+                  onClick={() => this.props.onIncrement(this.props.counter)}
                   className="btn btn-secondary btn-sm"
               >
                   increment
               </button>
               <button
-                  onClick={this.handleDelete}
+                  onClick={() => this.props.onDelete(this.props.counter.id)}
                   className="btn btn-danger btn-sm m-2"
               >
                   Delete
@@ -38,8 +25,13 @@ class Counter extends Component {
 
     getBadgeClasses(){
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
+    }
+
+    formatCount(){
+      const { value } = this.props.counter;
+      return value === 0 ? "Zero" : value
     }
 }
 
